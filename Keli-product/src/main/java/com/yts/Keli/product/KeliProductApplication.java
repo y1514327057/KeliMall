f26d35319b4011b9bc11d390c1b899a1bd2e620a
zke1514 <1514327057@qq.com>
2025-08-17 17:08:42 +0800

    网关配置 完成所有商品的分级查询

--- Keli-product/src/main/java/com/yts/Keli/product/KeliProductApplication.java
+++ Keli-product/src/main/java/com/yts/Keli/product/KeliProductApplication.java
@@ -3,7 +3,9 @@ package com.yts.Keli.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.yts.Keli.product.dao")
 @SpringBootApplication(scanBasePackages = {"com.yts.Keli.product"})
 public class KeliProductApplication {
